33217d659bd1eb26c7a282e18268de9af9aa9c68
Ian Macalinao <me@ian.pw>
2015-10-11 18:13:56 -0500

    Move test cards into example

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,28 @@ import ReactDOM from 'react-dom';
 import SwipeCards from 'react-swipe-cards';
 
 const App = React.createClass({
+
   render() {
-    return <SwipeCards />;
+
+    let cards = [];
+    for (let i = 0; i < 10; i++) {
+      cards.push({
+        name: 'Test',
+        age: 18,
+        friends: 3,
+        moments: 3,
+        picture: 'https://avatars3.githubusercontent.com/u/401263?v=3&s=460'
+      });
+    }
+
+    return (
+      <div style={{ width: 300, margin: '0px auto' }}>
+        <SwipeCards width={300} height={500} cards={cards} />
+      </div>
+    );
+
   }
+
 });
 
 ReactDOM.render(
