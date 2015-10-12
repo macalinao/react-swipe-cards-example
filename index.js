1857473415b9426fab313d5fa6e95cc196b8750c
Ian Macalinao <me@ian.pw>
2015-10-12 12:51:46 -0500

    Populate cards from backend

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
+let cards = [];
+for (let i = 0; i < 5; i++) {
+  cards.push({
+    name: 'Ian',
+    age: 18,
+    friends: 3,
+    moments: 3,
+    picture: 'https://avatars3.githubusercontent.com/u/401263?v=3&s=460'
+  });
+  cards.push({
+    name: 'Tejas',
+    age: 16,
+    friends: 1,
+    moments: 3,
+    picture: 'https://avatars1.githubusercontent.com/u/5959235?v=3&s=460'
+  });
+}
+
+
+app.get('/cards', (req, res) => {
+  res.json({
+    data: cards
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,30 @@
+import $ from 'jquery';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SwipeCards from 'react-swipe-cards';
 
+
 const App = React.createClass({
 
+  componentDidMount() {
+  },
+
   render() {
-
-    let cards = [];
-    for (let i = 0; i < 10; i++) {
-      cards.push({
-        name: 'Test',
-        age: 18,
-        friends: 3,
-        moments: 3,
-        picture: 'https://avatars3.githubusercontent.com/u/401263?v=3&s=460'
-      });
-    }
-
     return (
       <div style={{ width: 300, margin: '0px auto' }}>
-        <SwipeCards width={300} height={500} cards={cards} />
+        <SwipeCards width={300} height={500} cards={this.props.cards} />
       </div>
     );
-
   }
 
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('reactContainer')
-);
+$.get('/cards', (data) => {
+  ReactDOM.render(
+    <App cards={data.data} />,
+    document.getElementById('reactContainer')
+  );
+});
+
